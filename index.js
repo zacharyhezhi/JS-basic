d411204a1b478ec6e5472e9a4d930c28a7691413
Zachary <hezhibruno@gmail.com>
2020-01-26 22:19:04 +1100

    Array practice

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 console.log('Hello World');
 let firstName = 'Zachary', secondName = "He";
 console.log(firstName, secondName);
+
+let person = {
+    name: 'zachary',
+    age: 30
+};
+console.log(person);
+
+let selectedColor = ['red','blue'];
+selectedColor[2] = 1;
+console.log(selectedColor.length);
