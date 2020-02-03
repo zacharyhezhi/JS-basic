c73c010755751e6e77125507b2680492b4ca2e0b
Zachary <hezhibruno@gmail.com>
2020-02-03 15:10:57 +1100

    If else

--- index.js
+++ index.js
@@ -59,3 +59,20 @@ a = b;
 b = c;
 console.log(a);
 console.log(b);
+
+//If the hour is between 6am and 12am: Good Morning!
+//If it is between 12pm and 6pm: Good Afternoon!
+//Otherwise: Good evening!
+let hour = 12;
+
+if (hour >= 6 && hour <12) {
+    console.log('Good Morning!');
+}
+
+else if (hour >= 12 && hour < 18) 
+    console.log('Good Afternoon!');
+
+
+else {
+    console.log('Good evening!');
+}
