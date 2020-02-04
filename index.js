e627fb12c9cf2930c7f2a68f238bfc5cae16abb5
Zachary <hezhibruno@gmail.com>
2020-02-04 23:21:40 +1100

    Sum of Multiples of 3 and 5

--- index.js
+++ index.js
@@ -252,3 +252,17 @@ object1 = {
     director: 'b'
 };
 showProperties(object1);
+
+//Sum of Multiples of 3 and 5
+function sum(limit) {
+    let sum = 0;
+
+    for (let i = 0; i <= limit; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+console.log(sum(12));
