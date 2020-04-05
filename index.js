6310ca73b27db68189b9e541d895b0b8ebc2d4f4
Zachary <hezhibruno@gmail.com>
2020-04-05 22:05:25 +1000

    Exercise 1- Sum of Arguments

--- index.js
+++ index.js
@@ -929,3 +929,11 @@ playVideo.apply({ name: 'Mosh'}, [1, 2]);
 const fn = playVideo.bind({ name: 'Mosh'});// Three ways to bind this in old JS
 fn();
 
+//Exercise 1- Sum of Arguments
+function sum(...items) {
+    if (items.length === 1 && Array.isArray(items[0]))
+        items = [...items[0]];
+    return items.reduce((a, b) => a + b);
+}
+console.log(sum([1,2,3,4]));
+console.log(sum(1,2,3,4));
